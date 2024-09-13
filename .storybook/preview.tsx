8c48db6e6767a1508f9a5042f06ec4c608a7a4de
Junale <mr.simon.moller@gmail.com>
2024-09-13 19:01:47 +0200

    fix linting error

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,7 @@
 import type { Preview } from "@storybook/react";
-import '../src/index.css';
+import "../src/index.css";
 import React from "react";
-import { withThemeByClassName } from '@storybook/addon-themes';
+import { withThemeByClassName } from "@storybook/addon-themes";
 
 
 
@@ -13,17 +13,17 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-    layout: 'fullscreen',
+    layout: "fullscreen",
     options: {
       // The `a` and `b` arguments in this function have a type of `import('@storybook/types').IndexEntry`. Remember that the function is executed in a JavaScript environment, so use JSDoc for IntelliSense to introspect it.
       storySort: (a, b) => {
         // Prioritize docs stories
-        if (a.id.includes('docs') && !b.id.includes('docs')) {
+        if (a.id.includes("docs") && !b.id.includes("docs"))
           return -1;
-        }
-        if (!a.id.includes('docs') && b.id.includes('docs')) {
+
+        if (!a.id.includes("docs") && b.id.includes("docs"))
           return 1;
-        }
+
         // If both or neither are docs, sort alphabetically
         return a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true });
       },
@@ -31,23 +31,23 @@ const preview: Preview = {
   },
   decorators: [
     (Story, context) => {
-      const themeClass = context.globals.theme === 'dark' ? 'bg-black' : 'bg-white';
+      const themeClass = context.globals.theme === "dark" ? "bg-black" : "bg-white";
       return (
-        <div className={`flex flex-col w-screen h-screen justify-center items-center ${themeClass}`}>
+        <div className={"flex h-screen w-screen flex-col items-center justify-center " + themeClass}>
           <Story />
         </div>
       );
     },
     withThemeByClassName({
       themes: {
-        // nameOfTheme: 'classNameForTheme',
-        light: '',
-        dark: 'dark',
+        // NameOfTheme: 'classNameForTheme',
+        light: "",
+        dark: "dark",
       },
-      defaultTheme: 'light',
+      defaultTheme: "light",
     })
   ],
-  tags: ['autodocs'],
+  tags: ["autodocs"],
 };
 
 export default preview;
